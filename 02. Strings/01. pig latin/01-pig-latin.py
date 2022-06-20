131d48842f7d1d90bd17748616d5bcc389ea1cb0
Mahdi Jafari <mahdi100th@gmail.com>
2022-06-20 10:24:34 +0430

    Pig Latin

--- 02. Strings/01. pig latin/01-pig-latin.py
+++ 02. Strings/01. pig latin/01-pig-latin.py
@@ -1,12 +1,30 @@
-def pig_latin():
-    word = input("Enter an English word: ")
-    if word.startswith(("a", "e", "i", "o", "u")):
-        word = f"{word}way"
-    else:
-        word = f"{word[1:]}{word[0]}ay"
+# def pig_latin():
+#     """ My Solution
 
-    print(word)
-    return word
+#     Returns:
+#         string: word in pig latin
+#     """
+#     word = input("Enter an English word: ")
+#     if word.startswith(("a", "e", "i", "o", "u")):
+#         word = f"{word}way"
+#     else:
+#         word = f"{word[1:]}{word[0]}ay"
+
+#     print(word)
+#     return word
+
+# pig_latin()
 
 
-pig_latin()
+# def pig_latin(word):
+#     """The book solution
+
+#     Returns:
+#         string: word in pig latin
+#     """
+#     if word[0] in "aeiou":
+#         return f"{word}way"
+#     return f"{word[1:]}{word[0]}ay"
+
+
+# print(pig_latin('python'))
