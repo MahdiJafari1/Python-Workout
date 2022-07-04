6da84632bbf585165a34b6af14232e4fa6c8d5fe
Mahdi Jafari <mahdi100th@gmail.com>
2022-07-04 08:48:54 +0430

    reverse lines formatted

--- 05. Files/09_reverse_lines.py
+++ 05. Files/09_reverse_lines.py
@@ -2,4 +2,3 @@ def reverse_lines(infilename, outfilename):
     with open(infilename) as infile, open(outfilenam, "w") as outfile:
         for one_line in infile:
             outfile.write(f"{one_line.rstrip()[::-1]}\n")
-        
